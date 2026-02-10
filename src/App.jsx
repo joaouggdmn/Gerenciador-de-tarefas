162aadc2fa24a58287fbf7ac5c03a28dfbbcf4e9
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-02-10 11:17:54 -0300

    usando local storage para armazenar as tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/tasks";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar React",
-      description:
-        "Estudar os conceitos básicos de React, como componentes, props e estado.",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Fazer Exercícios",
-      description:
-        "Praticar os exercícios de React para reforçar o aprendizado.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Criar Projeto",
-      description:
-        "Desenvolver um projeto simples utilizando React para aplicar os conhecimentos adquiridos.",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(
+    () => localStorage.setItem("tasks", JSON.stringify(tasks)),
+    [tasks],
+  );
 
   //ATUALIZA O ESTADO DE UMA TAREFA
   function onTaskClick(taskId) {
