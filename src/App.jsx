dbf06f1ed0ad9f326933354d2cbe77123cc11435
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-02-11 00:44:39 -0300

    pegando tarefas de uma biblioteca para simular uma api restful

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,25 @@ import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || []
+    JSON.parse(localStorage.getItem("tasks")) || [],
   );
 
   useEffect(
     () => localStorage.setItem("tasks", JSON.stringify(tasks)),
     [tasks],
   );
+
+  useEffect(() => {
+    async function fetchTasks() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        { method: "GET" }
+      );
+      const data = await response.json();
+      setTasks(data);
+    }
+    fetchTasks();
+  }, []);
 
   //ATUALIZA O ESTADO DE UMA TAREFA
   function onTaskClick(taskId) {
@@ -45,7 +57,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="min-h-screen min-w-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           GERENCIADOR DE TAREFAS
