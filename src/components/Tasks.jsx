9e2a8832f3b5fd9d2721378d6e23ad672394a758
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-02-11 15:37:15 -0300

    criação de componentes para reduzir duplicidade de código

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,15 @@
 import { ChevronRight, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 function Tasks(props) {
   const navigate = useNavigate();
 
-  function onSeeDetails(task){
+  function onSeeDetails(task) {
     const query = new URLSearchParams();
     query.set("title", task.title);
     query.set("description", task.description);
-    
+
     navigate(`/task-details?${query.toString()}`);
   }
 
@@ -16,22 +17,26 @@ function Tasks(props) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button
+          <Button
             onClick={() => props.onTaskClick(task.id)}
             className={`text-left bg-slate-400 w-full text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}
           >
             {task.title}
-          </button>
+          </Button>
 
-          <button onClick={()=> onSeeDetails(task)} className="bg-slate-400 text-white p-2 rounded-md">
+          <Button
+            onClick={() => onSeeDetails(task)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
             <ChevronRight />
-          </button>
-          <button
+          </Button>
+
+          <Button
             onClick={() => props.onTaskDelete(task.id)}
             className="bg-slate-400 text-white p-2 rounded-md"
           >
             <TrashIcon />
-          </button>
+          </Button>
         </li>
       ))}
     </ul>
