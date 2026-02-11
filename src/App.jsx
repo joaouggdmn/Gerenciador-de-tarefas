621dfd8d34f5e172798ac6dde003af3298c41fe7
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-02-11 16:00:08 -0300

    remove fake restful api

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,6 @@ function App() {
     [tasks],
   );
 
-  useEffect(() => {
-    async function fetchTasks() {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=10",
-        { method: "GET" },
-      );
-      const data = await response.json();
-      setTasks(data);
-    }
-    if (tasks.length === 0) {
-      fetchTasks();
-    }
-  }, []);
-
   //ATUALIZA O ESTADO DE UMA TAREFA
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
